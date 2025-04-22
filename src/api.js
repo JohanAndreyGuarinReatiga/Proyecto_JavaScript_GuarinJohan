e5375243d778f3d027c74d1fb76b88af669e14d0
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-04-22 18:32:14 -0500

    feat: :construction: new feature creation visuals added and api functions

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 const API_BASE_URL = "https://www.dnd5eapi.co/api"
+const MOCK_API_URL = "https://67d2f8148bca322cc268a7de.mockapi.io"
 
-
+// api function
 async function fetchFromAPI(endpoint) {
   try {
     const response = await fetch(`${API_BASE_URL}/${endpoint}`)
@@ -12,6 +13,43 @@ async function fetchFromAPI(endpoint) {
     return await response.json()
   } catch (error) {
     console.error(`getting error ${endpoint}:`, error)
+    throw error
+  }
+}
+
+// mockapi 
+async function getAllCharacters() {
+  try {
+    const response = await fetch(`${MOCK_API_URL}/characters`)
+
+    if (!response.ok) {
+      throw new Error(`Error fetching characters: ${response.status}`)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error("Error getting characters from MockAPI:", error)
+    return []
+  }
+}
+
+async function createCharacter(characterData) {
+  try {
+    const response = await fetch(`${MOCK_API_URL}/characters`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(characterData),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Error creating character: ${response.status}`)
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error("Error creating character in MockAPI:", error)
     throw error
   }
 }
